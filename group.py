4e70e514f795f5d1e052b1c1b1eb3565986b4ab3
Toby Dixon <toby.dixon.23@ucl.ac.uk>
2023-11-02 18:58:36 +0100

    code for groups

--- group.py
+++ group.py
@@ -1,5 +1,13 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
 
-my_group =
+jill={"name":"jill","age":26,"job":"biologist","connections":{"friends":["zalika"],"partners":["john"]}}
+zalika={"name":"zalika","age":28,"job":"artist","connections":{"friends":["jill"]}}
+john = {"name":"john","age":27,"job":"writer","connections":{"partners":["jill"]}}
+nash = {"name":"nash","age":34,"job":"chef","connections":{"cousins":["john"],"tenant":["zalika"]}}
+
+my_group =[jill,zalika,john,nash]
+
+
+
+
